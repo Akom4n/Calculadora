a48ad54039203ba61cb6f456d0a45e4449e6a062
Vinicius Silva <xdenik13@gmail.com>
2024-09-23 13:46:48 -0300

    Melhoria visual

--- src/CalcPFX.java
+++ src/CalcPFX.java
@@ -1,27 +1,31 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class CalcPFX extends Application {
 
+    private double resultadoAnterior = 0;
+
     @Override
     public void start(Stage primaryStage) {
-        // Configurações da janela
         primaryStage.setTitle("Calculadora");
 
         // Criando os componentes da interface
         Label label1 = new Label("Primeiro número:");
-        Label label2 = new Label("Segundo número:");
         TextField num1Field = new TextField();
+        Label label2 = new Label("Segundo número:");
         TextField num2Field = new TextField();
         Label resultadoLabel = new Label("Resultado:");
         TextField resultadoField = new TextField();
         resultadoField.setEditable(false); // Campo não editável
+
+        CheckBox usarResultadoAnterior = new CheckBox("Usar resultado anterior");
 
         // Botões de operação
         Button btnSoma = new Button("Somar");
@@ -29,57 +33,71 @@ public class CalcPFX extends Application {
         Button btnMultiplicacao = new Button("Multiplicar");
         Button btnDivisao = new Button("Dividir");
 
-        // GridPane para organizar os componentes
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(8);
-        grid.setHgap(10);
+        // Layouts HBox para organizar os botões em uma linha
+        HBox hBoxBotoes = new HBox(10);  // Espaçamento de 10px entre os botões
+        hBoxBotoes.getChildren().addAll(btnSoma, btnSubtracao, btnMultiplicacao, btnDivisao);
 
-        // Adicionando os componentes no GridPane
-        grid.add(label1, 0, 0);
-        grid.add(num1Field, 1, 0);
-        grid.add(label2, 0, 1);
-        grid.add(num2Field, 1, 1);
-        grid.add(btnSoma, 0, 2);
-        grid.add(btnSubtracao, 1, 2);
-        grid.add(btnMultiplicacao, 0, 3);
-        grid.add(btnDivisao, 1, 3);
-        grid.add(resultadoLabel, 0, 4);
-        grid.add(resultadoField, 1, 4);
+        // VBox para organizar os componentes verticalmente
+        VBox vBoxLayout = new VBox(15);  // Espaçamento de 15px entre os componentes
+        vBoxLayout.setPadding(new Insets(15));
+        vBoxLayout.getChildren().addAll(
+                label1, num1Field,
+                label2, num2Field,
+                usarResultadoAnterior,
+                hBoxBotoes,
+                resultadoLabel,
+                resultadoField
+        );
 
         // Ações dos botões
-        btnSoma.setOnAction(e -> {
-            double num1 = Double.parseDouble(num1Field.getText());
-            double num2 = Double.parseDouble(num2Field.getText());
-            resultadoField.setText(String.valueOf(num1 + num2));
-        });
-
-        btnSubtracao.setOnAction(e -> {
-            double num1 = Double.parseDouble(num1Field.getText());
-            double num2 = Double.parseDouble(num2Field.getText());
-            resultadoField.setText(String.valueOf(num1 - num2));
-        });
-
-        btnMultiplicacao.setOnAction(e -> {
-            double num1 = Double.parseDouble(num1Field.getText());
-            double num2 = Double.parseDouble(num2Field.getText());
-            resultadoField.setText(String.valueOf(num1 * num2));
-        });
-
-        btnDivisao.setOnAction(e -> {
-            double num1 = Double.parseDouble(num1Field.getText());
-            double num2 = Double.parseDouble(num2Field.getText());
-            if (num2 != 0) {
-                resultadoField.setText(String.valueOf(num1 / num2));
-            } else {
-                resultadoField.setText("Erro: Divisão por zero");
-            }
-        });
+        btnSoma.setOnAction(e -> realizarOperacao(num1Field, num2Field, resultadoField, "+", usarResultadoAnterior));
+        btnSubtracao.setOnAction(e -> realizarOperacao(num1Field, num2Field, resultadoField, "-", usarResultadoAnterior));
+        btnMultiplicacao.setOnAction(e -> realizarOperacao(num1Field, num2Field, resultadoField, "*", usarResultadoAnterior));
+        btnDivisao.setOnAction(e -> realizarOperacao(num1Field, num2Field, resultadoField, "/", usarResultadoAnterior));
 
         // Cena e exibição
-        Scene scene = new Scene(grid, 300, 200);
+        Scene scene = new Scene(vBoxLayout, 400, 400);
+        scene.getStylesheets().add("style.css");  // Atualize o caminho para o arquivo CSS
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void realizarOperacao(TextField num1Field, TextField num2Field, TextField resultadoField, String operador, CheckBox usarResultadoAnterior) {
+        double num1 = getFirstNumber(num1Field, usarResultadoAnterior);
+        double num2 = Double.parseDouble(num2Field.getText());
+        double resultado = 0;
+
+        switch (operador) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    resultadoField.setText("Erro: Divisão por zero");
+                    return;
+                }
+                break;
+        }
+
+        resultadoField.getStyleClass().add("resultado-field");
+        resultadoField.setText(String.valueOf(resultado));
+        resultadoAnterior = resultado;
+    }
+
+    private double getFirstNumber(TextField num1Field, CheckBox usarResultadoAnterior) {
+        if (usarResultadoAnterior.isSelected()) {
+            return resultadoAnterior;
+        } else {
+            return Double.parseDouble(num1Field.getText());
+        }
     }
 
     public static void main(String[] args) {
